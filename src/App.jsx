3dcfc1dae552a34b0570c2319ffebb4333274d39
TheIntenseCoderAmit <amitpers321@gmail.com>
2025-02-26 01:29:15 +0530

    corrected contact spel

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
